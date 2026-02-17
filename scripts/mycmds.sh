552b7efaff4035d51eb0d7623ae462e95e383938
ribenit-com <zuandilong@gmail.com>
2026-02-17 17:52:13 +0900

    Update mycmds.sh

--- scripts/mycmds.sh
+++ scripts/mycmds.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
-# 一级菜单：分类选择
+# 一级菜单：功能分类
 
 categories_dir="$HOME/mycmds/categories"
 categories=($(ls "$categories_dir"))
 
-echo "== 一级菜单: 功能分类 =="
-i=1
-for cat in "${categories[@]}"; do
-    echo "$i) ${cat%.sh}"
-    ((i++))
+idx=0
+while true; do
+    clear
+    echo "== 一级菜单: 功能分类 =="
+    for i in "${!categories[@]}"; do
+        cat_name="${categories[$i]%.sh}"
+        if [ $i -eq $idx ]; then
+            echo -e "➤ $cat_name"
+        else
+            echo "  $cat_name"
+        fi
+    done
+    echo
+    echo "↑/↓选择  Enter进入分类  q退出"
+
+    read -rsn1 key
+    if [[ $key == $'\x1b' ]]; then
+        read -rsn2 key
+        if [[ $key == '[A' ]]; then
+            ((idx--))
+            [ $idx -lt 0 ] && idx=$((${#categories[@]}-1))
+        elif [[ $key == '[B' ]]; then
+            ((idx++))
+            [ $idx -ge ${#categories[@]} ] && idx=0
+        fi
+    elif [[ $key == "" ]]; then
+        bash "$categories_dir/${categories[$idx]}"
+    elif [[ $key == "q" ]]; then
+        clear
+        exit 0
+    fi
 done
-
-read -p "选择分类编号: " choice
-if ! [[ "$choice" =~ ^[0-9]+$ ]] || [ "$choice" -lt 1 ] || [ "$choice" -gt "${#categories[@]}" ]; then
-    echo "选择无效"
-    exit 1
-fi
-
-# 执行二级菜单脚本
-bash "$categories_dir/${categories[$((choice-1))]}"
